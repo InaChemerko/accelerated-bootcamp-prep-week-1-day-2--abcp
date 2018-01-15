740322fe784cd40dc2f20c86b9593b34743ed9e0
Ina Chemerko <innachemerko@gmail.com>
2018-01-15 18:37:57 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -9,7 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element) {
-
+var newArray = [...array,element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
